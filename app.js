c3db2866ece51d1e44b88b4bfe4bcd8a4a5ea067
Faysal Ahamed <faysal@webpers.com>
2018-04-16 19:36:10 +0600

    Queue added

---
+++ app.js
@@ -0,0 +1,14 @@
+
+var obj= Object.create(Object.prototype)
+
+obj['name']="Jobayer"
+obj['address']={arr:[1,2,3],info:"some info here",NID:"********"}
+console.log(obj);
+
+
+function sum(a,b){
+ return a+b;
+}
+
+
+
--- test.ts
+++ test.ts
@@ -19,4 +19,52 @@ class Test extends Base{
 
 const obj=new Test();
 
-console.log(obj.sum(0,0))
+console.log(obj.sum(10,10))
+
+
+
+
+function QUEUE(){
+    this.container=[];
+    this.pointer=null;
+    this.pop=function(){
+      if(this.container.length>0){
+        if(this.pointer===null){
+           console.log(this.container[this.container.length-1])
+           this.pointer=this.container.length-2
+        }
+        if(this.pointer<0){
+          console.log("Queue empty")
+          //return;
+        }
+        if(this.pointer>=0){
+          var temp=this.pointer;
+          console.log(this.container[this.pointer])
+          this.pointer=temp-1;
+        }
+        
+         //this.pointer=
+        
+      }else{
+        console.log("Queue is empty")
+      }
+    }
+    
+  }
+  
+  var obj=new QUEUE()
+  
+  // obj.container.push(1)
+  // obj.container.push(2)
+  // obj.container.push(3)
+  // obj.container.push(4)
+  // obj.container.push(4)
+  
+  obj.pop()
+  obj.pop()
+  obj.pop()
+  obj.pop()
+  
+  
+  
+  
